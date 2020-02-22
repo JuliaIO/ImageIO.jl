3d760bb75a610d783a3af94ebe10ac399da11c16
Ian <i.r.butterworth@gmail.com>
2020-02-22 14:43:57 -0500

    add tests for PNGFiles

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using Test
 using ImageIO
+using FileIO: File, DataFormat
 
-@testset "ImageIO" test
+@testset "ImageIO" begin
+
+    @testset "PNGs" begin
+        img = rand(UInt8, 10, 10)
+        f = File{DataFormat{:PNG}}("test.png")
+        ImageIO.save(f, img)
+        img_saveload = ImageIO.load(f)
+        @test all(img .== reinterpret(UInt8, img_saveload))
+    end
 
 end
+
+isfile("test.png") && rm("test.png")
