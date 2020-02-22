d9834605034d37601ce6b475641c6677c76d26cf
Ian <i.r.butterworth@gmail.com>
2020-02-22 14:34:01 -0500

    faster lazy loading of PNGFiles

--- src/ImageIO.jl
+++ src/ImageIO.jl
@@ -3,16 +3,22 @@ module ImageIO
 using FileIO: File, DataFormat
 
 ## PNGs
+const PNGFiles_LOADED = Ref(false)
+
 function load(f::File{DataFormat{:PNG}}; kwargs...)
-    !isdefined(ImageIO, :PNGFiles) && @eval ImageIO import PNGFiles
-    return PNGFiles.load(f.filename, kwargs...)
+    if !PNGFiles_LOADED[]
+        @eval ImageIO import PNGFiles
+        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
+    end
+    return Base.invokelatest(PNGFiles.load, f.filename, kwargs...)
 end
 function save(f::File{DataFormat{:PNG}}, image::S; kwargs...) where {
-        T,
-        S<:Union{AbstractMatrix, AbstractArray{T,3}}
-    }
-    !isdefined(ImageIO, :PNGFiles) && @eval ImageIO import PNGFiles
-    return PNGFiles.save(f.filename, image, kwargs...)
+                                T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
+    if !PNGFiles_LOADED[]
+        @eval ImageIO import PNGFiles
+        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
+    end
+    return Base.invokelatest(PNGFiles.save, f.filename, image, kwargs...)
 end
 
 end # module
