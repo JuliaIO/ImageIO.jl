2bdfd54add7a20cd64ef0ce330d55a68133150e0
Ian <i.r.butterworth@gmail.com>
2020-05-24 01:30:24 -0400

    tweak save via stream method

--- src/ImageIO.jl
+++ src/ImageIO.jl
@@ -27,21 +27,19 @@ function load(s::IO; kwargs...)
     return Base.invokelatest(PNGFiles.load, s, kwargs...)
 end
 
-function save(f::File{DataFormat{:PNG}}, image::S; kwargs...) where {
-                                T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
+function save(f::File{DataFormat{:PNG}}, image::S; kwargs...) where {T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
     if !PNGFiles_LOADED[]
         @eval ImageIO import PNGFiles
         PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
     end
     return Base.invokelatest(PNGFiles.save, f.filename, image, kwargs...)
 end
-function save(s::Stream{DataFormat{:PNG}}, image::S; kwargs...) where {
-                                T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
+function save(s::Stream{DataFormat{:PNG}}, image::S; kwargs...) where {T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
     if !PNGFiles_LOADED[]
         @eval ImageIO import PNGFiles
         PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
     end
-    return Base.invokelatest(PNGFiles.save, s, image, kwargs...)
+    return Base.invokelatest(PNGFiles.save, stream(s), image, kwargs...)
 end
 
 end # module
