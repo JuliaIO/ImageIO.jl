9ab4ca3d6308c1bde0673ea5fef4d99a7a2a871c
Simon <sdanisch@protonmail.com>
2020-05-25 16:05:48 +0200

    slightly cleaner loading

--- src/ImageIO.jl
+++ src/ImageIO.jl
@@ -3,41 +3,35 @@ module ImageIO
 using FileIO: File, DataFormat, Stream, stream
 
 ## PNGs
-const PNGFiles_LOADED = Ref(false)
 
 function load(f::File{DataFormat{:PNG}}; kwargs...)
-    if !PNGFiles_LOADED[]
-        @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
-    end
-    return Base.invokelatest(PNGFiles.load, f.filename, kwargs...)
+    return Base.invokelatest(PNGFiles().load, f.filename, kwargs...)
 end
+
 function load(s::Stream{DataFormat{:PNG}}; kwargs...)
-    if !PNGFiles_LOADED[]
+    if !isdefined(ImageIO, :PNGFiles)
         @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
     end
     return Base.invokelatest(PNGFiles.load, stream(s), kwargs...)
 end
+
 function load(s::IO; kwargs...)
-    if !PNGFiles_LOADED[]
+    if !isdefined(ImageIO, :PNGFiles)
         @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
     end
     return Base.invokelatest(PNGFiles.load, s, kwargs...)
 end
 
 function save(f::File{DataFormat{:PNG}}, image::S; kwargs...) where {T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
-    if !PNGFiles_LOADED[]
+    if !isdefined(ImageIO, :PNGFiles)
         @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
     end
     return Base.invokelatest(PNGFiles.save, f.filename, image, kwargs...)
 end
+
 function save(s::Stream{DataFormat{:PNG}}, image::S; kwargs...) where {T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
-    if !PNGFiles_LOADED[]
+    if !isdefined(ImageIO, :PNGFiles)
         @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
     end
     return Base.invokelatest(PNGFiles.save, stream(s), image, kwargs...)
 end
