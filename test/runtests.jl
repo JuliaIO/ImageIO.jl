6c10c60e76990a50da2607b1880ec19eba8825db
Ian <i.r.butterworth@gmail.com>
2020-10-24 10:59:06 -0400

    make CI multithreaded

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,19 @@ using ImageIO
 using FileIO: File, DataFormat, Stream, @format_str
 
 tmpdir = mktempdir()
+Threads.nthreads() <= 1 && @info "Threads.nthreads() = $(Threads.nthreads()), multithread tests will be disabled"
 @testset "ImageIO" begin
 
     @testset "PNGs" begin
 
-        @testset "Threaded save" begin
-            # test that loading of PNGFiles happens sequentially and doesn't segfault
-            img = rand(UInt8, 10, 10)
-            Threads.@threads for i in 1:Threads.nthreads()
-                f = File{DataFormat{:PNG}}(joinpath(tmpdir, "test_fpath_$i.png"))
-                ImageIO.save(f, img)
+        if Threads.nthreads() > 1
+            @testset "Threaded save" begin
+                # test that loading of PNGFiles happens sequentially and doesn't segfault
+                img = rand(UInt8, 10, 10)
+                Threads.@threads for i in 1:Threads.nthreads()
+                    f = File{DataFormat{:PNG}}(joinpath(tmpdir, "test_fpath_$i.png"))
+                    ImageIO.save(f, img)
+                end
             end
         end
         img = rand(UInt8, 10, 10)
