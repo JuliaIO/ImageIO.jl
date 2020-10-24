3ef6615fb6a0077b48b9c9e8939f4e4ba8a98f26
Ian <i.r.butterworth@gmail.com>
2020-10-24 08:55:30 -0400

    do checked_import sequentially

--- src/ImageIO.jl
+++ src/ImageIO.jl
@@ -2,43 +2,38 @@ module ImageIO
 
 using FileIO: File, DataFormat, Stream, stream
 
+const load_locker = Base.ReentrantLock()
+
+function checked_import(pkg::Symbol)
+    lock(load_locker) do
+        if !isdefined(ImageIO, pkg)
+            @eval ImageIO import $pkg
+        end
+    end
+    return nothing
+end
+
 ## PNGs
-const PNGFiles_LOADED = Ref(false)
 
 function load(f::File{DataFormat{:PNG}}; kwargs...)
-    if !PNGFiles_LOADED[]
-        @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
-    end
+    checked_import(:PNGFiles)
     return Base.invokelatest(PNGFiles.load, f.filename, kwargs...)
 end
 function load(s::Stream{DataFormat{:PNG}}; kwargs...)
-    if !PNGFiles_LOADED[]
-        @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
-    end
+    checked_import(:PNGFiles)
     return Base.invokelatest(PNGFiles.load, stream(s), kwargs...)
 end
 function load(s::IO; kwargs...)
-    if !PNGFiles_LOADED[]
-        @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
-    end
+    checked_import(:PNGFiles)
     return Base.invokelatest(PNGFiles.load, s, kwargs...)
 end
 
 function save(f::File{DataFormat{:PNG}}, image::S; kwargs...) where {T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
-    if !PNGFiles_LOADED[]
-        @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
-    end
+    checked_import(:PNGFiles)
     return Base.invokelatest(PNGFiles.save, f.filename, image, kwargs...)
 end
 function save(s::Stream{DataFormat{:PNG}}, image::S; kwargs...) where {T, S<:Union{AbstractMatrix, AbstractArray{T,3}}}
-    if !PNGFiles_LOADED[]
-        @eval ImageIO import PNGFiles
-        PNGFiles_LOADED[] = isdefined(ImageIO, :PNGFiles)
-    end
+    checked_import(:PNGFiles)
     return Base.invokelatest(PNGFiles.save, stream(s), image, kwargs...)
 end
 
