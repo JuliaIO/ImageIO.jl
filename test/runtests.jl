b1f091aa9adb5f4b13750e0080946303be3abe2f
Ian <i.r.butterworth@gmail.com>
2021-02-21 01:56:14 -0500

    test tidy and TODOs

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ Threads.nthreads() <= 1 && @info "Threads.nthreads() = $(Threads.nthreads()), mu
 @testset "ImageIO" begin
 
     @testset "PNGs" begin
-        for typ in [0:1, UInt8, N0f8, Gray{N0f8}, Gray{Float64}, RGB{N0f8}, RGB{Float64}]
+        for typ in [UInt8, N0f8, Gray{N0f8}, RGB{N0f8}] # TODO: Fix 0:1, Gray{Float64}, RGB{Float64} in PNGFiles
             @testset "$typ PNG" begin
                 img = rand(typ, 10, 10)
                 f = File{DataFormat{:PNG}}(joinpath(tmpdir, "test_fpath.png"))
@@ -42,19 +42,45 @@ Threads.nthreads() <= 1 && @info "Threads.nthreads() = $(Threads.nthreads()), mu
     end
 
     @testset "Portable bitmap" begin
-        for typ in [0:1, N0f8, Gray{N0f8}, Gray{Float64}, RGB{N0f8}, RGB{Float64}]
-            @testset "$typ pgm" begin
-                img = rand(typ, 10, 10)
-                for fmt in (format"PGMBinary", format"PGMText")
-                    f = File{fmt}(joinpath(tmpdir, "test_fpath.pgm"))
-                    ImageIO.save(f, img)
-                    img_saveload = ImageIO.load(f)
-                    @test img == img_saveload
+        @testset "Bicolor pbm" begin
+            img = rand(0:1, 10, 10)
+            for fmt in (format"PBMBinary", format"PBMText")
+                f = File{fmt}(joinpath(tmpdir, "test_fpath.pbm"))
+                ImageIO.save(f, img)
+                img_saveload = ImageIO.load(f)
+                @test img == img_saveload
 
-                    open(io->ImageIO.save(Stream(fmt, io), img), joinpath(tmpdir, "test_io.pgm"), "w")
-                    img_saveload = open(io->ImageIO.load(Stream(fmt, io)), joinpath(tmpdir, "test_io.pgm"))
-                    @test img == img_saveload
-                end
+                open(io->ImageIO.save(Stream(fmt, io), img), joinpath(tmpdir, "test_io.pbm"), "w")
+                img_saveload = open(io->ImageIO.load(Stream(fmt, io)), joinpath(tmpdir, "test_io.pbm"))
+                @test img == img_saveload
+            end
+        end
+
+        @testset "Gray pgm" begin
+            img = rand(N0f8, 10, 10)
+            for fmt in (format"PGMBinary", format"PGMText")
+                f = File{fmt}(joinpath(tmpdir, "test_fpath.pgm"))
+                ImageIO.save(f, img)
+                img_saveload = ImageIO.load(f)
+                @test img == img_saveload
+
+                open(io->ImageIO.save(Stream(fmt, io), img), joinpath(tmpdir, "test_io.pgm"), "w")
+                img_saveload = open(io->ImageIO.load(Stream(fmt, io)), joinpath(tmpdir, "test_io.pgm"))
+                @test img == img_saveload
+            end
+        end
+
+        @testset "Color ppm" begin
+            img = rand(RGB{N0f8}, 10, 10)
+            for fmt in (format"PPMBinary", format"PPMText")
+                f = File{fmt}(joinpath(tmpdir, "test_fpath.ppm"))
+                ImageIO.save(f, img)
+                img_saveload = ImageIO.load(f)
+                @test img == img_saveload
+
+                open(io->ImageIO.save(Stream(fmt, io), img), joinpath(tmpdir, "test_io.ppm"), "w")
+                img_saveload = open(io->ImageIO.load(Stream(fmt, io)), joinpath(tmpdir, "test_io.ppm"))
+                @test img == img_saveload
             end
         end
     end
