c54b3ab5a3d86f2869aa4dbfa2304f1213048c21
Johnny Chen <johnnychen94@hotmail.com>
2021-05-22 16:45:43 +0800

    ImageIO v0.0.1
    This is an empty package only for compatibility

--- src/ImageIO.jl
+++ src/ImageIO.jl
@@ -1,18 +1,3 @@
 module ImageIO
 
-using FileIO: File, DataFormat
-
-## PNGs
-function load(f::File{DataFormat{:PNG}}; kwargs...)
-    !isdefined(ImageIO, :PNGFiles) && @eval ImageIO import PNGFiles
-    return PNGFiles.load(f.filename, kwargs...)
-end
-function save(f::File{DataFormat{:PNG}}, image::S; kwargs...) where {
-        T,
-        S<:Union{AbstractMatrix, AbstractArray{T,3}}
-    }
-    !isdefined(ImageIO, :PNGFiles) && @eval ImageIO import PNGFiles
-    return PNGFiles.save(f.filename, image, kwargs...)
-end
-
 end # module
